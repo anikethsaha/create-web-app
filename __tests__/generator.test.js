cc1cf41ec87cbc6e9dc80cb92e6f6694a621a38a
anikethsaha <anik220798@gmail.com>
2019-05-27 17:25:13 +0530

    refactor

--- __tests__/generator.test.js
+++ __tests__/generator.test.js
@@ -9,16 +9,18 @@ describe('generator-trial:app', () => {
     return helpers
       .run(path.join(__dirname, '../generators/app'))
       .withPrompts({
-        scaffold_name: 'testing',
-        description: 'Simple Scaffold',
-        main: 'index.js',
-        github_repo: '',
-        author: '',
+        projectName: 'testing',
+        description: 'Simple project',
+        author: 'BuildTop',
+        stack: 1,
+        bundler: 2,
+        version: '',
+        githubRepo: "",
         license: ''
       });
   });
 
   it('creates files', () => {
-    assert.file(['webpack-scaffold-testing']);
+    assert.file(['dist/testing']);
   });
 });
