6bd7a85f4f5b3e6a8ed0d9d293f6f5b2c0414bbd
anikethsaha <anik220798@gmail.com>
2019-06-12 22:36:41 +0530

    fix(list): fixed the path issue in list command

--- src/commands/list.js
+++ src/commands/list.js
@@ -2,9 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const chalk_1 = require("chalk");
 const glob = require("glob-all");
+const path = require("path");
 exports.default = () => {
     // tslint:disable-next-line: no-console
     process.stdout.write(`\n${chalk_1.default.bgGreen.bold(`List of All Templates Available`)} \n`);
-    const listTemplate = glob.sync("generators/app/templates/*")
+    const listTemplate = glob.sync(`${path.resolve(__dirname + "../../generators/app/templates").trim()}/*`)
         .map((p, i) => process.stdout.write(`${i} ${p.split("/").pop()} \n`));
 };
--- src/commands/list.ts
+++ src/commands/list.ts
@@ -1,8 +1,9 @@
 import chalk from "chalk";
 import * as glob from "glob-all";
+import * as path from "path"
 export default (): void => {
 // tslint:disable-next-line: no-console
   process.stdout.write(`\n${chalk.bgGreen.bold(`List of All Templates Available`)} \n`);
-  const listTemplate = glob.sync("generators/app/templates/*")
+  const listTemplate = glob.sync(`${path.resolve(__dirname + "../../generators/app/templates").trim()}/*`)
 	.map((p: string, i: number) => process.stdout.write(`${i} ${p.split("/").pop()} \n`));
 };
